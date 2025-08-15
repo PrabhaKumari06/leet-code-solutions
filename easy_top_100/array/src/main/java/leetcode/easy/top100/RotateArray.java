3ac13463058418b228f467945460375be91dc7c0
Prabha <prabhakumari06@yahoo.com>
2025-08-15 22:16:29 +0200

    duplicat,rotate,single number

---
+++ easy_top_100/array/src/main/java/leetcode/easy/top100/RotateArray.java
@@ -0,0 +1,22 @@
+package leetcode.easy.top100;
+
+public class RotateArray {
+    public int[] rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+        return nums;
+
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
