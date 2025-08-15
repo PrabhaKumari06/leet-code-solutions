532f2a7f10e4884a221763adf2098d64802c2e8a
Prabha <prabhakumari06@yahoo.com>
2025-08-15 17:13:31 +0200

    Remove Duplicates from Sorted Array

--- easy_top_100/array/src/main/java/leetcode/easy/top100/RemoveDuplicatesFromSortedArray.java
+++ easy_top_100/array/src/main/java/leetcode/easy/top100/RemoveDuplicatesFromSortedArray.java
@@ -1,12 +1,7 @@
 package leetcode.easy.top100;
 
 public class RemoveDuplicatesFromSortedArray {
-    public static void main(String[] args) {
-        int[] input = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        System.out.println(" count of unique no : " + removeDuplicates(input));
-
-    }
-    public static int removeDuplicates(int[] nums) {
+    public int removeDuplicates(int[] nums) {
         int index = 0;
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] != nums[i + 1])
